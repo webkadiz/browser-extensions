4b1ecc85d58676f8614c4eecc29a0772b10cb25a
Webkadiz <webkadiz@gmail.com>
2021-01-09 05:44:06 +0300

    returned a functionality - movement from task input which TickTick deleted it for some reason

--- ticktick/shortcuts.js
+++ ticktick/shortcuts.js
@@ -1,3 +1,8 @@
+new MutationObserver(appShowUpHandler)
+    .observe(document.querySelector("#root"), {
+        childList: true
+    })
+
 window.addEventListener("keydown", e => {
     if (e.ctrlKey) {
         if (e.code === "KeyB" && e.shiftKey) {
@@ -10,8 +15,10 @@ window.addEventListener("keydown", e => {
         }
         if (e.code === "KeyF") {
             e.preventDefault()
-            if (document.querySelector(".CodeMirror textarea"))
+            if (document.querySelector(".CodeMirror textarea")) {
+                setTaskInputMovingHandler()
                 document.querySelector(".CodeMirror textarea").focus()
+            }
         }
         if (e.code === "KeyD") { // focus on editor
             e.preventDefault()
@@ -20,3 +27,34 @@ window.addEventListener("keydown", e => {
         }
     }
 })
+
+function appShowUpHandler() {
+    setTaskInputMovingHandler()
+}
+
+function setTaskInputMovingHandler() {
+    let 
+        codeMirror = document.querySelector(".CodeMirror textarea"),
+        taskInput = document.querySelector(".task-input")
+
+    if (taskInput && codeMirror.onfocus === null) {
+        codeMirror.onfocus = () => {
+            console.log("focus")
+            document.querySelector(".task-input").classList.add("focus")
+            document.querySelector(".task.active")?.classList.remove("active")
+            document.querySelector(".task.selected")?.classList.remove("selected")
+            window.addEventListener("keydown", taskInputMovingHandler)
+        }
+    }
+}
+
+function taskInputMovingHandler(e) {
+    if (e.code === "ArrowDown") {
+        document.querySelector(".task").classList.add("active")
+        document.querySelector(".task").classList.add("selected")
+        document.querySelector(".task .inline-editor div").focus()
+
+        document.querySelector(".task-input").classList.remove("focus")
+        window.removeEventListener("keydown", taskInputMovingHandler)
+    }
+}
