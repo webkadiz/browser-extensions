ee44fb48137daa9568273369d31562732d89aef7
Webkadiz <webkadiz@gmail.com>
2021-02-01 15:27:54 +0300

    query all secondary elements

--- youtube/content.js
+++ youtube/content.js
@@ -1,9 +1,11 @@
 window.addEventListener("keydown", e => {
     if (e.ctrlKey && e.shiftKey && e.code === "KeyY") {
-        if (document.querySelector("#secondary").style.display === "none") {
-            document.querySelector("#secondary").style.display = ""
-        } else {
-            document.querySelector("#secondary").style.display = "none"
-        }
+        document.querySelectorAll("#secondary").forEach(sidebar => {
+            if (sidebar.style.display === "none") {
+                sidebar.style.display = ""
+            } else {
+                sidebar.style.display = "none"
+            }
+        })
     }
 })
